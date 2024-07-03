1e8726f698ba1542a0c7cb3f6e43cb71d8f44f37
KhadeejaArshadAli <Khadijaarshad435@gmail.com>
2024-07-03 16:12:35 +0500

    done

---
+++ calculator/src/OperationButton.js
@@ -0,0 +1,7 @@
+//OPERATION
+import { ACTIONS } from "./App"
+
+
+export default function OperationButton({ dispatch, operation }) {
+    return (<button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>{operation}</button>)
+}
